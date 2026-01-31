31bbe677a3f3b79832e2b22d5d9e7eedbf726106
freddie <freddie@philaudley.co.uk>
2026-01-31 20:33:04 +0000

    Added a typedef for struct block

--- src/slib/src/memory/malloc.c
+++ src/slib/src/memory/malloc.c
@@ -5,34 +5,34 @@
 
 void* head = NULL;
 
-struct block {
+typedef struct {
     int free;
     size_t size;
-    struct block* next;
-    struct block* prev;
+    block* next;
+    block* prev;
     void* ptr;
     char data[];
-};
+} block;
 
 
 void find_block(size_t size) {
-    struct block* current_block = head;
+    block* current_block = head;
     while (current_block && !(current_block -> free && current_block -> size >= size)) {
         current_block = current_block -> next;
     }
 }
 
 
-void split_block(struct block* block, size_t size) {
-    struct block* new_block = (struct block*) (block -> data + size);
+void split_block(block* block_arg, size_t size) {
+    block* new_block = (block*) (block_arg -> data + size);
     new_block -> free = 1;
-    new_block -> size = block -> size - size - BLOCK_DATA_SIZE;
-    new_block -> next = block -> next;
-    new_block -> prev = block;
-    new_block -> ptr = new_block -> data;
+    new_block -> size = block_arg -> size - size - BLOCK_DATA_SIZE;
+    new_block -> next = block_arg -> next;
+    new_block -> prev = block_arg;
+    new_block -> ptr = block_arg -> data;
 
-    block -> next = new_block;
-    block -> size = size;
+    block_arg -> next = new_block;
+    block_arg -> size = size;
 
     if (new_block -> next) {
         new_block -> next -> prev = new_block;
