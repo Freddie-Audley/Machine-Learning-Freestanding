230726009f50e22e8239453f983882b8d4fcef9b
freddie <freddie@philaudley.co.uk>
2026-02-01 17:40:10 +0000

    Rebuilt debug

--- cmake-build-debug/CMakeFiles/main.dir/src/main.c.o.d
+++ cmake-build-debug/CMakeFiles/main.dir/src/main.c.o.d
@@ -1,4 +1,2 @@
 CMakeFiles/main.dir/src/main.c.o: \
- /home/freddie/CLionProjects/Machine-Learning-Freestanding/src/main.c \
- /home/freddie/CLionProjects/Machine-Learning-Freestanding/include/slib/maths.h \
- /home/freddie/CLionProjects/Machine-Learning-Freestanding/include/slib/io.h
+ /home/freddie/CLionProjects/Machine-Learning-Freestanding/src/main.c
--- cmake-build-debug/src/slib/CMakeFiles/slib.dir/src/io/print.c.o.d
+++ cmake-build-debug/src/slib/CMakeFiles/slib.dir/src/io/print.c.o.d
@@ -1,5 +1,5 @@
 src/slib/CMakeFiles/slib.dir/src/io/print.c.o: \
  /home/freddie/CLionProjects/Machine-Learning-Freestanding/src/slib/src/io/print.c \
- /usr/include/stdc-predef.h \
  /home/freddie/CLionProjects/Machine-Learning-Freestanding/include/slib/io.h \
- /home/freddie/CLionProjects/Machine-Learning-Freestanding/include/slib/string.h
+ /home/freddie/CLionProjects/Machine-Learning-Freestanding/include/slib/string.h \
+ /home/freddie/CLionProjects/Machine-Learning-Freestanding/include/slib/definitions.h
