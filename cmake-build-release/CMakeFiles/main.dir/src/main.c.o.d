e38aa4168d287f9e05c1eee01f5bfd3aeb46c6d5
freddie <freddie@philaudley.co.uk>
2026-02-01 20:59:01 +0000

    Created umbrella header slib.h

--- cmake-build-release/CMakeFiles/main.dir/src/main.c.o.d
+++ cmake-build-release/CMakeFiles/main.dir/src/main.c.o.d
@@ -1,4 +1,2 @@
 CMakeFiles/main.dir/src/main.c.o: \
- /home/freddie/CLionProjects/Machine-Learning-Freestanding/src/main.c \
- /home/freddie/CLionProjects/Machine-Learning-Freestanding/include/slib/memory.h \
- /home/freddie/CLionProjects/Machine-Learning-Freestanding/include/slib/definitions.h
+ /home/freddie/CLionProjects/Machine-Learning-Freestanding/src/main.c
---
+++ include/slib/slib.h
@@ -0,0 +1,10 @@
+#ifndef SLIB_H
+#define SLIB_H
+
+#include "slib/definitions.h"
+#include "slib/io.h"
+#include "slib/maths.h"
+#include "slib/memory.h"
+#include "slib/string.h"
+
+#endif
