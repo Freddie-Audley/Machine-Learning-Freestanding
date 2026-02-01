0f658706794867319acf8526fb08b8a3502b0929
freddie <freddie@philaudley.co.uk>
2026-02-01 17:40:29 +0000

    Commit unversioned files from debug rebuild

---
+++ cmake-build-debug/src/slib/CMakeFiles/slib.dir/src/memory/free.c.o.d
@@ -0,0 +1,5 @@
+src/slib/CMakeFiles/slib.dir/src/memory/free.c.o: \
+ /home/freddie/CLionProjects/Machine-Learning-Freestanding/src/slib/src/memory/free.c \
+ /home/freddie/CLionProjects/Machine-Learning-Freestanding/include/slib/memory.h \
+ /home/freddie/CLionProjects/Machine-Learning-Freestanding/include/slib/definitions.h \
+ /home/freddie/CLionProjects/Machine-Learning-Freestanding/src/slib/src/memory/malloc.c
