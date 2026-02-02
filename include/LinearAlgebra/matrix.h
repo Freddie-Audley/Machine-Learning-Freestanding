60812cf9b3791bd5e3ef325f1f60ea2b12d7a966
freddie <freddie@philaudley.co.uk>
2026-02-02 00:14:36 +0000

    Added matrix_add, matrix_multiply and matrix_transpose

--- include/LinearAlgebra/matrix.h
+++ include/LinearAlgebra/matrix.h
@@ -3,8 +3,12 @@
 typedef struct {
     unsigned int rows;
     unsigned int cols;
-    double** data;
+    double* data;
 } Matrix;
 
 Matrix matrix_create(unsigned int rows, unsigned int cols);
 void matrix_free(Matrix* matrix);
+
+Matrix matrix_add(Matrix* lhs, Matrix* rhs);
+Matrix matrix_multiply(Matrix* lhs, Matrix* rhs);
+Matrix matrix_transpose(Matrix* matrix);
--- src/LinearAlgebra/src/matrix.c
+++ src/LinearAlgebra/src/matrix.c
@@ -1,29 +1,15 @@
 #include "slib/slib.h"
 #include "LinearAlgebra/matrix.h"
 
+
 Matrix matrix_create(unsigned int rows, unsigned int cols) {
     Matrix matrix;
     matrix.rows = rows;
     matrix.cols = cols;
-    matrix.data = malloc(rows * sizeof(double*));
+    matrix.data = malloc(rows * cols * sizeof(double));
 
     if (!matrix.data) {
         matrix.rows = matrix.cols = 0;
-        return matrix;
-    }
-
-    for (int i = 0; i < rows; i++) {
-        matrix.data[i] = malloc(cols * sizeof(double));
-
-        if (!matrix.data[i]) {
-            for (int j = 0; j < i; j++) {
-                free(matrix.data[j]);
-            }
-            free(matrix.data);
-            matrix.data = NULL;
-            matrix.rows = matrix.cols = 0;
-            return matrix;
-        }
     }
 
     return matrix;
@@ -33,11 +19,51 @@ Matrix matrix_create(unsigned int rows, unsigned int cols) {
 void matrix_free(Matrix* matrix) {
     if (!matrix || !matrix -> data) return;
 
-    for (int i = 0; i < matrix -> rows; i++) {
-        free(matrix -> data[i]);
-    }
-
     free(matrix -> data);
     matrix -> data = NULL;
     matrix -> rows = matrix -> cols = 0;
+}
+
+
+Matrix matrix_add(Matrix* lhs, Matrix* rhs) {
+    Matrix result = matrix_create(lhs -> rows, lhs -> cols);
+
+    for (int i = 0; i < lhs -> rows; i++) {
+        for (int j = 0; j < lhs -> cols; j++) {
+            unsigned int index = i * lhs -> cols + j;
+            result.data[index] = lhs -> data[index] + rhs -> data[index];
+        }
+    }
+
+    return result;
+}
+
+
+Matrix matrix_multiply(Matrix* lhs, Matrix* rhs) {
+    Matrix result = matrix_create(lhs -> rows, rhs -> cols);
+
+    for (int i = 0; i < lhs -> rows; i++) {
+        for (int j = 0; j < rhs -> cols; j++) {
+            double sum = 0.0;
+            for (int k = 0; k < lhs -> cols; k++) {
+                sum += lhs -> data[i * lhs -> cols + k] * rhs -> data[k * rhs -> cols + j];
+            }
+            result.data[i * lhs -> cols + j] = sum;
+        }
+    }
+
+    return result;
+}
+
+
+Matrix matrix_transpose(Matrix* matrix) {
+    Matrix result = matrix_create(matrix -> cols, matrix -> rows);
+
+    for (int i = 0; i < matrix -> rows; i++) {
+        for (int j = 0; j < matrix -> cols; j++) {
+            result.data[j * matrix -> cols + i] = matrix -> data[i * matrix -> cols + j];
+        }
+    }
+
+    return result;
 }
