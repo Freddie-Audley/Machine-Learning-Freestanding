283831c2664c34a46a5a992d94aceb205940c4e1
freddie <freddie@philaudley.co.uk>
2026-05-14 00:19:44 +0100

    Swapped arguments in scalar multiplication functions. Edited indentation in CMake files

---
+++ cmake-build-debug/src/slib/CMakeFiles/slib.dir/src/io/print.c.o.d
@@ -0,0 +1,5 @@
+src/slib/CMakeFiles/slib.dir/src/io/print.c.o: \
+ /home/freddie/CLionProjects/Machine-Learning-Freestanding/src/slib/src/io/print.c \
+ /home/freddie/CLionProjects/Machine-Learning-Freestanding/include/slib/io.h \
+ /home/freddie/CLionProjects/Machine-Learning-Freestanding/include/slib/definitions.h \
+ /home/freddie/CLionProjects/Machine-Learning-Freestanding/include/slib/string.h
--- src/slib/src/memory/allocate_memory.c
+++ src/slib/src/memory/allocate_memory.c
@@ -5,117 +5,170 @@
 #define PROT_WRITE 0x2
 #define MAP_PRIVATE 0x2
 #define MAP_ANONYMOUS 0x20
-#define MAP_FAILED ((void*)-1)
+#define MAP_FAILED ((void*) - 1)
 
 #define ALIGNMENT 8
 #define align(x) (((x) + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1))
+
+#define PAGE_SIZE 4096
 #define BLOCK_HEADER_SIZE align(sizeof(Block))
+#define ARENA_HEADER_SIZE align(sizeof(Arena))
+#define MIN_BLOCK_SIZE 16
+
 
 typedef struct Block {
     int free;
     size_t size;
     struct Block* next;
     struct Block* prev;
+    struct Block* next_free;
+    struct Block* prev_free;
+    struct Arena* arena;
     char data[];
 } Block;
 
-static Block* head = NULL;
+
+typedef struct Arena {
+    size_t size;
+    struct Arena* next;
+    Block* first;
+} Arena;
 
 
-Block* find_block(Block** tail, size_t size) {
-    Block* current_block = head;
-    *tail = NULL;
+static Arena* arena_head = NULL;
+static Block* free_list = NULL;
 
-    while (current_block && !(current_block -> free && current_block -> size >= size)) {
-        *tail = current_block;
-        current_block = current_block -> next;
+
+static void free_list_insert(Block* block) {
+    block -> next_free = free_list;
+    block -> prev_free = NULL;
+
+    if (free_list) {
+        free_list -> prev_free = block;
     }
 
-    return current_block;
+    free_list = block;
 }
 
 
-void split_block(Block* block, size_t size) {
+static void free_list_remove(Block* block) {
+    if (block -> prev_free) {
+        block -> prev_free -> next_free = block -> next_free;
+    } else {
+        free_list = block -> next_free;
+    }
+
+    if (block -> next_free) {
+        block -> next_free -> prev_free = block -> prev_free;
+    }
+}
+
+
+static Arena* new_arena(size_t min_size) {
+    size_t total_size = ARENA_HEADER_SIZE + BLOCK_HEADER_SIZE + min_size;
+    size_t arena_size = PAGE_SIZE;
+
+    while (arena_size < total_size) {
+        arena_size += PAGE_SIZE;
+    }
+
+    Arena* arena = mmap(NULL, arena_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (arena == MAP_FAILED) {
+        return NULL;
+    }
+
+    arena -> size = arena_size;
+    arena -> next = arena_head;
+    arena_head = arena;
+
+    Block* block = (Block*) ((char*) arena + ARENA_HEADER_SIZE);
+    block -> size = arena_size - ARENA_HEADER_SIZE - BLOCK_HEADER_SIZE;
+    block -> free = 1;
+    block -> next = NULL;
+    block -> prev = NULL;
+    block -> next_free = NULL;
+    block -> prev_free = NULL;
+    block -> arena = arena;
+
+    arena -> first = block;
+
+    free_list_insert(block);
+
+    return arena;
+}
+
+
+static Block* find_block(size_t size) {
+    Block* block = free_list;
+
+    while (block) {
+        if (block -> size >= size) return block;
+        block = block -> next;
+    }
+
+    return NULL;
+}
+
+
+static void split_block(Block* block, size_t size) {
+    if (block -> size < size + BLOCK_HEADER_SIZE + MIN_BLOCK_SIZE) return;
+
     Block* new_block = (Block*) (block -> data + size);
     new_block -> free = 1;
     new_block -> size = block -> size - size - BLOCK_HEADER_SIZE;
     new_block -> next = block -> next;
     new_block -> prev = block;
-
-    block -> next = new_block;
-    block -> size = size;
+    new_block -> next_free = NULL;
+    new_block -> prev_free = NULL;
+    new_block -> arena = block -> arena;
 
     if (new_block -> next) {
         new_block -> next -> prev = new_block;
     }
+
+    block -> size = size;
+    block -> next = new_block;
+    free_list_insert(new_block);
 }
 
 
-Block* extend_heap(Block* tail, size_t size) {
-    Block* new_block = mmap(NULL, BLOCK_HEADER_SIZE + size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    if (new_block == MAP_FAILED) {
-        return NULL;
+static Block* merge_with_next(Block* block) {
+    Block* next = block -> next;
+
+    if (!next || !next -> free) {
+        return block;
     }
 
-    new_block -> size = size;
-    new_block -> free = 0;
-    new_block -> next = NULL;
-    new_block -> prev = tail;
+    free_list_remove(next);
 
-    if (tail) {
-        tail -> next = new_block;
-    }
-
-    return new_block;
-}
-
-
-void* allocate_memory(size_t size) {
-    if (size == 0) return NULL;
-
-    Block* new_block;
-    size_t const aligned_size = align(size);
-
-    if (head) {
-        Block* tail = head;
-        new_block = find_block(&tail, aligned_size);
-
-        if (new_block) {
-            if (new_block -> size - aligned_size >= BLOCK_HEADER_SIZE + ALIGNMENT) {
-                split_block(new_block, aligned_size);
-            }
-            new_block -> free = 0;
-
-        } else {
-            new_block = extend_heap(tail, aligned_size);
-            if (!new_block) {
-                return NULL;
-            }
-        }
-
-    } else {
-        new_block = extend_heap(NULL, aligned_size);
-        if (!new_block) {
-            return NULL;
-        }
-        head = new_block;
-    }
-
-    return new_block -> data;
-}
-
-
-Block* merge_blocks(Block* block) {
-    if (block -> next && block -> next -> free) {
-        block -> size += BLOCK_HEADER_SIZE + block -> next -> size;
-        block -> next = block -> next -> next;
-    }
+    block -> size += BLOCK_HEADER_SIZE + next -> size;
+    block -> next = next -> next;
 
     if (block -> next) {
         block -> next -> prev = block;
     }
 
     return block;
+}
+
+
+void* allocate_memory(size_t size) {
+    if (size == 0) return NULL;
+
+    size = align(size);
+
+    Block* block = find_block(size);
+    if (!block) {
+        Arena* arena = new_arena(size);
+        if (!arena) return NULL;
+        block = arena -> first;
+    }
+
+    free_list_remove(block);
+    split_block(block, size);
+    block -> free = 0;
+
+    return block -> data;
 }
 
 
@@ -126,19 +179,28 @@ void free(void* ptr) {
     block -> free = 1;
 
     if (block -> prev && block -> prev -> free) {
-        block = merge_blocks(block -> prev);
+        block = merge_with_next(block -> prev);
     }
 
     if (block -> next && block -> next -> free) {
-        block = merge_blocks(block);
+        block = merge_with_next(block);
     }
 
-    if (!block -> next) {
-        if (block -> prev) {
-            block -> prev -> next = NULL;
-        } else {
-            head = NULL;
+    Arena* arena = block -> arena;
+    if (!block -> prev && !block -> next) {
+        Arena** head_ptr_ptr = &arena_head;
+
+        while (*head_ptr_ptr && *head_ptr_ptr != arena) {
+            head_ptr_ptr = &(*head_ptr_ptr) -> next;
         }
-        munmap(block, BLOCK_HEADER_SIZE + block -> size);
+
+        if (*head_ptr_ptr == arena) {
+            *head_ptr_ptr = arena -> next;
+        }
+
+        munmap(arena, arena -> size);
+        return;
     }
+
+    free_list_insert(block);
 }
